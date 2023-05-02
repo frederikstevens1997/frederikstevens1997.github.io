941245f722ec00c1b1e9b1997c02de103261302d
Frederik Stevens <frederik.stevens@gmail.com>
2023-05-02 09:57:53 +0200

    all but font is done

--- assets/javascript/travelmatrix.js
+++ assets/javascript/travelmatrix.js
@@ -29,8 +29,12 @@ function displayMatchingTravelInfo() {
   if (from === to) {
     // if so, display a message and return early
     document.getElementById("div_to_change").innerHTML = "The origin and destination must be different.";
-    var div = document.querySelector(".bars");
-    div.style.display = "none";
+    var fdiv = document.querySelector(".fbars");
+    fdiv.style.display = "none";
+    var bhdiv = document.querySelector(".bars-header");
+    bhdiv.style.display = "none";
+    var sdiv = document.querySelector(".sbars");
+    sdiv.style.display = "none";
     return;
   }
 
@@ -64,29 +68,39 @@ function displayMatchingTravelInfo() {
               const emissionPlane = parseFloat(info.trav[5]["emission-plane"]);
               const emissionAlt = parseFloat(info.trav[6]["emission-alt"]);
               const emissionCalc = emissionPlane - emissionAlt;
-              resultStr += `<p>By doing so <b>you would be saving ${emissionCalc} kg of CO2 emissions per person</b> compared to using the plane (which would create ${emissionPlane} kg of CO2 emissions per person).</p>`;
+              resultStr += `<p>By doing so you will save ${emissionCalc}kg of CO2 emissions per person compared to travelling by plane, which would generate ${emissionPlane}kg of CO2 emissions per person.</p>`;
               var f_div = document.querySelector(".fdiv");
               f_div.style.width =  emissionAlt/2000 * 75 + "%";
               var s_div = document.querySelector(".sdiv");
               s_div.style.width =  emissionPlane/2000 * 75 + "%";
+              var f_txt = document.getElementById("ftxt");
+              var s_txt = document.getElementById("stxt");
+              const fTextNode = document.createTextNode(emissionAlt + "kg of CO2 emissions:");
+              const sTextNode = document.createTextNode(emissionPlane + "kg of CO2 emissions:");
+              f_txt.appendChild(fTextNode);
+              s_txt.appendChild(sTextNode);
               var fbar = document.querySelector(".fbars");
               fbar.style.display = "block";
+              var title_bar = document.querySelector(".bars-header");
+              title_bar.style.display = "block";
+              var sbar = document.querySelector(".sbars");
+              sbar.style.display = "block";
             } else {
               const emissionAlt = parseFloat(info.trav[6]["emission-alt"]);
               emission_alt = emissionAlt;
-              resultStr += `<p>Given the short distance of this travel, taking the train should be the default means of transportation. Your emissions per person would be ${emissionAlt} kg of CO2.</p>`;
-              var div = document.querySelector(".fbars");
-              div.style.display = "none";
+              resultStr += `<p>Given the short distance of this travel, train should be the default means of transportation. Your emissions per person would be ${emissionAlt}kg of CO2.</p>`;
+              var fdiv = document.querySelector(".fbars");
+              fdiv.style.display = "none";
+              var bhdiv = document.querySelector(".bars-header");
+              bhdiv.style.display = "none";
+              var sdiv = document.querySelector(".sbars");
+              sdiv.style.display = "none";
             }
           });
         }
 
         // display the matching travel information in the "results" div
         document.getElementById("div_to_change").innerHTML = resultStr;
-        var title_bar = document.querySelector(".bars-header");
-        title_bar.style.display = "block";
-        var sbar = document.querySelector(".sbars");
-        sbar.style.display = "block";
       })
       .catch(error => console.error('Error loading travel information:', error));
 }
